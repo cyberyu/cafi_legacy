6983c4eeeac3dad775a7f213d49b707d55bae685
Shi Yu <shee.yu@gmail.com>
2015-09-16 03:13:21 +0100

    google search

--- backend/google/CustomSearchAPI/simple.py
+++ backend/google/CustomSearchAPI/simple.py
@@ -32,7 +32,7 @@ def main():
   # Build a service object for interacting with the API. Visit
   # the Google APIs Console <http://code.google.com/apis/console>
   # to get an API key for your own application.
-  service = build("customsearch", "v1",
+  service = build("CustomSearchAPI", "v1",
             developerKey="AIzaSyDkxRofVDMMrci34b9yFCePgJz2YAoDmAA")
 
 
