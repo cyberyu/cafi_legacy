54017fc01268ef639c5107e5dd59a8454d20eda6
tanmoypatra <tanmoy.patra009@gmail.com>
2015-10-07 20:53:30 +0530

    Add TikaExtractCookie.py (Shi Yu version) 	*Modification of the prior code TikaExtract 	*Considers browser cookie details using mechanize 	*Takes in url or path and directly parses. No Download
    	renamed:    TikaExtract.py -> TikaExtractCookie.py

--- backend/google/Extract_Text/TikaExtractCookie.py
+++ backend/google/Extract_Text/TikaExtractCookie.py
@@ -1,4 +1,5 @@
 __author__ = 'tanmoy'
+__author__ = 'shiyu'
 
 #!/usr/bin/env python2.7
 import tika
@@ -7,11 +8,43 @@ import ExcelToCsv as xc
 import requests
 import os
 import sys
+import mechanize
+import cookielib
 
-class DocumentConvertor :
+
+TIKA_SERVICE = 'http://localhost:1234/tika'
+target_path="./data/"   #Change target path to the folder where files will be stored
+
+class DocumentConvertor:
+
+    # Browser
+    br = mechanize.Browser()
+
+    # Cookie Jar
+    cj = cookielib.LWPCookieJar()
+    br.set_cookiejar(cj)
+    br.set_handle_equiv(True)
+    #br.set_handle_gzip(True)
+    br.set_handle_redirect(True)
+    br.set_handle_referer(True)
+    br.set_handle_robots(False)
+
+    # Follows refresh 0 but not hangs on refresh > 0
+    br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
+
+    # Want debugging messages?
+    #br.set_debug_http(True)
+    #br.set_debug_redirects(True)
+    #br.set_debug_responses(True)
+
+    # User-Agent (this is cheating, ok?)
+    br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
+
     def __init__(self,url):
         self.url = url
         self.parsed_json = self.document_to_text(url)
+
+
 
     def document_to_text(self,url):
 
@@ -37,16 +70,16 @@ class DocumentConvertor :
     #using tika as service check readme for updates
 
     def tikaParser(self,url):
-        parsed = parser.from_file(url,'http://localhost:1234/tika') # address of the local host created by tika jar
+        parsed = parser.from_file(url,TIKA_SERVICE) # address of the local host created by tika jar
         return parsed
 
     def tikaParserExcel(self,url):
-        parsed = parser.from_file(url)
-
-        if parsed["content"] == None:
+        parsed = parser.from_file(url, TIKA_SERVICE)
+        #print unicode(parsed["content"]).strip()
+        if parsed["content"] == None:  # need more general format
             # Open the url provided as an argument to the function and read the content
             if str(url).startswith('http://') or str(url).startswith('https://'):
-                base = "./data/"
+                base = target_path
                 f = requests.get(url, stream=True)
                 file_path= os.path.basename(url)
                 xls_file = base + ''.join([file_path.split('.')[0],'.xls'])
@@ -58,19 +91,21 @@ class DocumentConvertor :
             else:
                 xls_file = url
 
-            csv_file = xc.csv_from_excel(xls_file)
-            parsed1 = parser.from_file(csv_file,'http://localhost:1234/tika')
-            os.remove(csv_file)
-            os.remove(xls_file)
+            csv_file = xc.csv_from_excel(xls_file,target_path)
+            parsed1 = parser.from_file(csv_file,TIKA_SERVICE)
+            #os.remove(csv_file)
+            #os.remove(xls_file)
             return parsed1
+        elif unicode(parsed["content"]).strip().startswith("Access denied"):
+            return self.tikaParserCookie(url)
+
         else:
             return parsed
 
-if __name__ == "__main__":
-    #Testcases:
-    #url= "http://www.shareholder.com/visitors/activeedgardoc.cfm?f=xls&companyid=AAPL&id=10916067"
-    #url="/Users/tanmoy/Downloads/CAFI/APPLE_8-A12B.xls"  #Can accept both file path and url
-    url_or_path = raw_input("Enter path or url to extract text content: ")
-    query1 = DocumentConvertor(url_or_path)
-    print unicode(query1.parsed_json['content'])
+    # simulate a browser to extract the text with cookie
+    def tikaParserCookie(self, url):
+        r = self.br.retrieve(url)
+        query2 = parser.from_file(r[0],TIKA_SERVICE)
+        os.remove(r[0])
+        return query2
 
