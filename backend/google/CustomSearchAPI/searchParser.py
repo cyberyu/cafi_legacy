3ca5189eb943ae22bdc9465dbd3fc6f4e014afb9
tanmoypatra <tanmoy.patra009@gmail.com>
2015-10-14 00:08:51 +0530

    Fix: Bug fixed for single word quotation
    	modified:   google/CustomSearchAPI/searchParser.py

--- backend/google/CustomSearchAPI/searchParser.py
+++ backend/google/CustomSearchAPI/searchParser.py
@@ -20,7 +20,9 @@ class SearchQueryParser:
         self._parser = self.parser()
         self.parsed = []
         self.genString = "" #generating string back from Data Structure
-        self.keywords =[]
+        self.keywords = set()
+        self.keywordsRemove = set() #remove keywords which appear alone in a quotation
+        self.keywordsFinal = set() #Add the variable to display keywords maintaining the inherent structure of phrase
 
     def parser(self):
         """
@@ -36,118 +38,107 @@ class SearchQueryParser:
         - if an operator is missing, use an '&' operator
         & (And) |(Or)
         """
-        opOr = Forward()
+        operatorOr = Forward()
 
-        opWord = Group(Combine(Word(alphanums) + Suppress('*'))).setResultsName('wordwildcard') | \
+        operatorWord = Group(Combine(Word(alphanums) + Suppress('*'))).setResultsName('wordwildcard') | \
                             Group(Word(alphanums)).setResultsName('word')
-        opQuotesContent = Forward()
-        opQuotesContent << (
-            (opWord + opQuotesContent) | opWord
+
+        operatorQuotesContent = Forward()
+        operatorQuotesContent << (
+            (operatorWord + operatorQuotesContent) | operatorWord
         )
 
-        opQuotes = Group(
-            Suppress('"') + opQuotesContent + Suppress('"')
-        ).setResultsName("quotes") | opWord
+        operatorQuotes = Group(
+            Suppress('"') + operatorQuotesContent + Suppress('"')
+        ).setResultsName("quotes") | operatorWord
 
-        opParenthesis = Group(
-            (Suppress("(") + opOr + Suppress(")"))
-        ).setResultsName("parenthesis") | opQuotes
+        operatorParenthesis = Group(
+            (Suppress("(") + operatorOr + Suppress(")"))
+        ).setResultsName("parenthesis") | operatorQuotes
 
-        opNot = Forward()
-        opNot << (Group(
-            Suppress(Keyword("not", caseless=True)) + opNot
-        ).setResultsName("not") | opParenthesis)
+        operatorNot = Forward()
+        operatorNot << (Group(
+            Suppress(Keyword("not", caseless=True)) + operatorNot
+        ).setResultsName("not") | operatorParenthesis)
 
-        opAnd = Forward()
-        opAnd << (Group(
-            opNot + Suppress(Keyword("&", caseless=True)) + opAnd
+        operatorAnd = Forward()
+        operatorAnd << (Group(
+            operatorNot + Suppress(Keyword("&", caseless=True)) + operatorAnd
         ).setResultsName("&") | Group(
-            opNot + OneOrMore(~oneOf("& |") + opAnd)
-        ).setResultsName("&") | opNot)
+            operatorNot + OneOrMore(~oneOf("& |") + operatorAnd)
+        ).setResultsName("&") | operatorNot)
 
-        opOr << (Group(
-            opAnd + Suppress(Keyword("|", caseless=True)) + opOr
-        ).setResultsName("|") | opAnd)
+        operatorOr << (Group(
+            operatorAnd + Suppress(Keyword("|", caseless=True)) + operatorOr
+        ).setResultsName("|") | operatorAnd)
 
-        return opOr.parseString
+        return operatorOr.parseString
 
     def generateAnd(self, argument):
         x = self.generate(argument[0])
-        if len(argument[0])==1:
-            self.keywords.extend(argument[0])
         self.genString += " & "
         y = self.generate(argument[1])
-        if len(argument[1])==1:
-            self.keywords.extend(argument[1])
         return x.intersection(y)
 
     def generateOr(self, argument):
 
         x = self.generate(argument[0])
-        if len(argument[0])==1:
-            self.keywords.extend(argument[0])
         self.genString += " |"
         y = self.generate(argument[1])
-        if len(argument[1])==1:
-            self.keywords.extend(argument[1])
         return x.union(y)
 
     def generateNot(self, argument):
-        self.genString += "Not "
-        if len(argument[0])==1:
-            self.keywords.extend(argument[0])
+        self.genString += "not "
+        self.keywordsRemove.update(argument[0])
         return self.GetNot(self.generate(argument[0]))
 
     def generateParenthesis(self, argument):
         self.genString += "("
         x = self.generate(argument[0])
-        if len(argument[0])==1:
-            self.keywords.extend(argument[0])
         self.genString += ")"
         return x
 
     def generateQuotes(self, argument):
-        """generate quoted strings
-
-        It does an 'and' on the indidual search terms
+        """ generate quoted strings
+            It does an 'and' on the indidual search terms
         """
         r = Set()
         search_terms = []
         self.genString += "\""
+
         for item in argument:
             search_terms.append(item[0])
             if len(r) == 0:
                 r = self.generate(item)
             else:
                 r = r.intersection(self.generate(item))
+            if len(argument)>1:
+                self.keywordsRemove.update(item)
 
         self.genString += "\""
-        self.keywords .extend([" ".join(search_terms)])
-        return self.GetQuotes(' '.join(search_terms), r)
-        #' '.join(search_terms)
+        return self.GetQuotes(" ".join(search_terms), r)
 
     def generateWord(self, argument):
         self.genString += " "+argument[0]
-        if len(argument[0])==1:
-            self.keywords.extend(argument[0])
         return self.GetWord(argument[0])
 
     def generateWordWildcard(self, argument):
         self.genString += " "+ argument[0]+'* '
-        if len(argument[0])==1:
-            self.keywords.extend(argument[0])
         return self.GetWordWildcard(argument[0])
 
     def generate(self, argument):
         return self._methods[argument.getName()](argument)
 
     def GetWord(self, word):
+        self.keywords.update([word])
         return Set()
 
     def GetWordWildcard(self, word):
+        self.keywords.update([word])
         return Set()
 
     def GetQuotes(self, search_string, tmp_result):
+        self.keywords.update([search_string])
         return Set()
 
     def GetNot(self, not_set):
@@ -157,6 +148,9 @@ class SearchQueryParser:
         self.genString=""
         self.parsed = self._parser(query)[0]
         self.generate(self._parser(query)[0])
+        self.keywordsFinal= self.keywords
+        self.keywordsFinal.difference_update(self.keywordsRemove)
+        self.keywordsFinal = list(self.keywordsFinal) #Converting it to a list
         return self.parsed,self.genString
 
 class ParserTest():
@@ -168,14 +162,17 @@ class ParserTest():
     def Test(self):
 
         query = SearchQueryParser()
-        item = "(lawsuit* | court* | violation & greed failure |\"joint venture\"|\"teaming agreement\"|\"memorandom of understanding\"| illegal | regulation* | defandant)"
+        #item = "(lawsuit* | court* | violation & greed failure |\"joint venture\"|\"teaming agreement\"|\"memorandom of understanding\"| illegal | regulation* | defandant)"
+        item = "(\"joint venture\" | \"jv\" | \"mou\" | \"memorandum of understanding\" | \"strategic alliance\" | \"teaming agreement\" | \"strategic partner*\" | \"partner\" | \"supplier\" | \"provider\" | \"agreement\" | \"contract\" | \"component\" | \"subcontract*\" | \"receive\" | \"win*\")&\"Microsoft\""
+        #item = "(\"joint venture\" | hello |\"mou\")&\"Microsoft\""
+
         print "Input Query:"+item
         ParsedList,reGengString = query.Parse(item)
         print "Parsed List:",
         print ParsedList #Parsed List from query
         print "ReGenerated String:"+reGengString #regenerated string from the parsed list
-        print "Keywords:",
-        print query.keywords
+        print "Keywords Final:",
+        print query.keywordsFinal
         '''
         ##Use of pickle to serialize a complex list
         pickle.dump(query, file('parsedList.pickle','w'))
