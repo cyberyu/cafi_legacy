aa461110151db7626246194e3c1ee811d13e6252
tanmoypatra <tanmoy.patra009@gmail.com>
2015-10-16 07:02:26 +0530

    Update tabs and replace other &, >, < characters
    	modified:   ../../../static/js/textToHtml.js

--- static/js/textToHtml.js
+++ static/js/textToHtml.js
@@ -2,7 +2,8 @@
  * Created by tanmoy on 16/10/15.
  */
 function textToHtml(text) {
-	var textlines = text.split("\n");
+
+	var textlines = text.replace("&","&amp;").replace("\t","&nbsp;&nbsp;&nbsp;&nbsp;").replace("<","&lt;").replace(">","&gt;").split("\n");
 	var i;
 	var htmlfriend = "";
 	for (i=0; i<textlines.length; ++i) {
