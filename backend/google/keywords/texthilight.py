e4f0f4bcc302503b826ca399b2f87dcb79cdc26d
cyberyu <shee.yu@gmail.com>
2015-10-16 20:27:29 -0500

    removed a bug limiting text highlight to more than 1 keyword only

--- backend/google/keywords/texthilight.py
+++ backend/google/keywords/texthilight.py
@@ -10,7 +10,7 @@ class Highlighter:
 
     def highlight (self, text, searchstr, namevariations=None):
         kwlist = self.parseSearchString(searchstr)
-
+        #print kwlist
         if namevariations!=None:
             kwfinal = self.union(kwlist, namevariations)
             return self.addcolor(text,kwfinal)
@@ -20,10 +20,10 @@ class Highlighter:
     def addcolor (self, text, kwlist):
         regmatch = '(\\b' + '\\b)|(\\b'.join(kwlist) + '\\b)'
         regex = re.compile(regmatch, re.I)
-
+        #print regmatch
         i=0; output=""
 
-        if len(regex.findall(text))>1:  # when there is matched keywords, add color
+        if len(regex.findall(text))>=1:  # when there is matched keywords, add color
             i = 0; output = ""
             for m in regex.finditer(text):
                 output += "".join([text[i:m.start()],
@@ -48,18 +48,20 @@ class Highlighter:
 if __name__ == "__main__":
 
     h = Highlighter()
-    istring = "(\"joint venture\")&\"Deloitte\""
+    #istring = "(\"joint venture\")&\"IBM\""
+    #istring = "(\"joint venture\")"
+    istring = "(\"IBM end\")"
     # istring = 'Deloitte'
     #istring = "(\"Lisp Perl Python\")&\"Deloitte\""
-    newqstr = istring[:istring.rfind("&")]
-    newqstr = newqstr.replace('\"','')
+    #newqstr = istring[:istring.rfind("&")]
+    #newqstr = newqstr.replace('\"','')
     # newqstr = istring
 
 
-    hiqueryStr= newqstr
+    hiqueryStr= istring
 
     #hiqueryStr2 =
-    text = "Mastek and Deloitte End Joint Venture"
+    text = "Mastek and Deloitte and ibm End Joint Venture"
 
     #text = """Graham says that Perl is cooler than Java and Python than Perl. In some circles, maybe. Graham uses the example of Slashdot, written in Perl. But what about Advogato, written in C? What about all of the cool P2P stuff being written in all three of the languages? Considering that Perl is older than Java, and was at one time the Next Big Language, I think you would have a hard time getting statistical evidence that programmers consider Perl "cooler" than Java, except perhaps by virtue of the fact that Java has spent a few years as the "industry standard" (and is thus uncool for the same reason that the Spice Girls are uncool) and Perl is still "underground" (and thus cool, for the same reason that ambient is cool). Python is even more "underground" than Perl (and thus cooler?). Maybe all Graham has demonstrated is that proximity to Lisp drives a language underground. Except that he's got the proximity to Lisp argument backwards too."""
     print h.highlight(text, hiqueryStr, None)
