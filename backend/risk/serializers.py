3cd7db233457e316c3fe36c1ae8417501bc9b9e4
Mingfeng Yang <mfyang@gmail.com>
2015-10-17 02:58:48 -0400

    change output of risk_item serializer

--- backend/risk/serializers.py
+++ backend/risk/serializers.py
@@ -43,33 +43,32 @@ class ContentTypeRelatedField(serializers.RelatedField):
     def to_representation(self, value):
         return value.name
 
-class RiskRelatedField(serializers.RelatedField):
-    def to_internal_value(self, data):
-        return Risk.objects.get(pk=data)
-
-    def to_representation(self, value):
-        return value.name
-
-class CompanyRelatedField(serializers.RelatedField):
-    def to_internal_value(self, data):
-        return Company.objects.get(pk=data)
-
-    def to_representation(self, value):
-        return value.name
 
 class RiskItemSerializer(serializers.ModelSerializer):
     content_object = ContentObjectRelatedField(queryset=RiskItem.objects.all(), required=False)
     content_type = ContentTypeRelatedField(queryset=RiskItem.objects.all())
-    risk = RiskRelatedField(queryset=RiskItem.objects.all())
-    from_company = CompanyRelatedField(queryset=RiskItem.objects.all())
-    to_company = CompanyRelatedField(queryset=RiskItem.objects.all(), required=False)
 
     class Meta:
         model = RiskItem
-        # fields = ('risk', 'project', 'from_company', 'to_company', 'object_id', 'content_object')
 
     def save(self, **kwargs):
         object_id = self.validated_data.get('object_id')
         content_type = self.validated_data.get('content_type')
         content_object = content_type.get_object_for_this_type(pk=object_id)
         super(RiskItemSerializer, self).save(**kwargs)
+
+    def to_representation(self, obj):
+        if obj.to_company:
+            to_company = obj.to_company.name
+        else:
+            to_company = ''
+
+        return {
+            'project': obj.project.id,
+            'from_company': obj.from_company.name,
+            'risk': obj.risk.name,
+            'object_id': obj.object_id,
+            'content_type': obj.content_type.id,
+            'id': obj.id,
+            'to_company': to_company,
+        }
