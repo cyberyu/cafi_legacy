05543cbc938ca7f76e7aa1399c4ae9b0f48e6d06
Mingfeng Yang <mfyang@gmail.com>
2015-11-20 17:59:52 -0500

    get one more page

--- backend/google/tasks.py
+++ backend/google/tasks.py
@@ -43,7 +43,7 @@ service = build("customsearch", "v1", developerKey="AIzaSyBeoj7no9n3EfELeBGujKdS
 collection = service.cse()
 
 @shared_task(default_retry_delay=3, max_retries=3)
-def do_search(search,num_requests):
+def do_search(search, num_requests):
     # https://developers.google.com/custom-search/json-api/v1/reference/cse/list
     search_engine_id = '012608441591405123751:clhx3wq8jxk'
     counter = 0
