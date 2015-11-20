e89c24003b41f6bf84c9c376506be061ea367407
Mingfeng Yang <mfyang@gmail.com>
2015-11-20 14:24:28 -0500

    show all markers on map

--- static/js/map-controller.js
+++ static/js/map-controller.js
@@ -3,133 +3,36 @@
  */
 
 projectControllers.controller('mapCtrl', function($scope, uiGmapGoogleMapApi, uiGmapIsReady){
+  $scope.map = {center: {latitude: 40.1451, longitude: -99.6680}, zoom: 4};
+  $scope.options = {scrollwheel: false};
 
-    uiGmapGoogleMapApi.then(function (maps) {
-        //$scope.googlemap = {};
-        $scope.map = {
-            center: {
-                latitude: 40.1451,
-                longitude: -99.6680
-            },
-            zoom: 4,
-            pan: 1,
-            options: $scope.mapOptions,
-            control: {},
-            events: {
-                tilesloaded: function (maps, eventName, args) {},
-                dragend: function (maps, eventName, args) {},
-                zoom_changed: function (maps, eventName, args) {}
-            }
-        };
+  $scope.markers = [];
+  uiGmapIsReady.promise().then(function(){
+    angular.forEach($scope.addresses, function(address){
+      marker = {
+        id: address.id,
+        coords: { "latitude": address.lat, "longitude": address.lng},
+        info: address.name + ': ' + address.address
+      };
+      $scope.markers.push(marker);
     });
-    $scope.options = {scrollwheel: false};
-    $scope.marker = {
-        title: 'Address',
-        address: "",
-        coords: {
-            latitude: 40.1451,
-            longitude: -99.6680
-        },
-        visible: false,
-        id: 0
-    };
-    $scope.windowOptions = {
-        show:false
-    };
 
-    $scope.windowOptions1 = {
-        visible: false
-    };
+  });
 
-    $scope.onClick1 = function () {
-        $scope.windowOptions.visible = !$scope.windowOptions.visible;
-    };
-
-    $scope.onClick = function (data) {
-        console.log(data);
-        $scope.windowOptions.show = !$scope.windowOptions.show;
-        console.log('$scope.windowOptions.show: ', $scope.windowOptions.show);
-        console.log('Office Name ' + data);
-        //alert('This is a ' + data);
-    };
-    $scope.info = "Bug! Info issue"; // Trying to set in onclick, but it doesn't reflect
-
-    $scope.closeClick1 = function () {
-        $scope.windowOptions.visible = false;
-    };
-
-    $scope.closeClick = function () {
-        $scope.windowOptions.show = false;
-    };
-
-    uiGmapIsReady.promise() // if no value is put in promise() it defaults to promise(1)
-        .then(function (instances) {
-            console.log(instances[0].map); // get the current map
-        })
-        .then(function () {
-            $scope.markers = [];
-            for (var i = 0; i < $scope.addresses.length; i++) {
-                $scope.markers.push({
-                    id: $scope.markers.length,
-                    coords: {
-                        latitude: $scope.addresses[i].lat,
-                        longitude: $scope.addresses[i].lng
-                    },
-                    data: $scope.addresses[i].name
-                });
-            }
-            $scope.addMarkerClickFunction($scope.markers);
-        });
-
-    $scope.addMarkerClickFunction = function (markersArray) {
-        angular.forEach(markersArray, function (value, key) {
-            console.log(value);
-            value.onClick = function () {
-                $scope.info = value.data;
-                $scope.onClick(value.data);
-                $scope.MapOptions.markers.selected = value;
-            };
-        });
-    };
-    $scope.MapOptions = {
-        minZoom: 3,
-        zoomControl: false,
-        draggable: true,
-        navigationControl: false,
-        mapTypeControl: false,
-        scaleControl: false,
-        streetViewControl: false,
-        disableDoubleClickZoom: false,
-        keyboardShortcuts: true,
-        markers: {
-            selected: {}
-        },
-        styles: [{
-            featureType: "poi",
-            elementType: "labels",
-            stylers: [{
-                visibility: "off"
-            }]
-        }, {
-            featureType: "transit",
-            elementType: "all",
-            stylers: [{
-                visibility: "off"
-            }]
-        }],
-    };
-
-
-    $scope.locateOnMap = function(address){
-        console.log("Individual Marker");
-        $scope.marker.coords.latitude = address.lat;
-        $scope.marker.coords.longitude = address.lng;
-        $scope.map.center.latitude = address.lat;
-        $scope.map.center.longitude = address.lng;
-        $scope.map.zoom = 4;
-        $scope.marker.title = address.name;
-        $scope.marker.address = address.address;
-
-    };
-
+  $scope.locateOnMap = function(address){
+    //$scope.marker.coords.latitude = address.lat;
+    //$scope.marker.coords.longitude = address.lng;
+    $scope.markers.map(function(marker){
+      marker.icon = 'http://maps.google.com/mapfiles/ms/icons/red-dot.png';
+    });
+    angular.forEach($scope.markers, function(marker){
+      if (marker.id == address.id){
+        marker.icon = 'http://maps.google.com/mapfiles/ms/icons/green-dot.png';
+      }
+    });
+    $scope.map.center.latitude = address.lat;
+    $scope.map.center.longitude = address.lng;
+    $scope.map.zoom = 4;
+    $scope.title = address.name;
+  }
 });
