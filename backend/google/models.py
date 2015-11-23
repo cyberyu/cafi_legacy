ae4957525df3f8b1fccc3ba8ffc1b301904241bc
Mingfeng Yang <mfyang@gmail.com>
2015-11-23 13:52:08 -0500

    add two fields for relevance classification

--- backend/google/models.py
+++ backend/google/models.py
@@ -54,6 +54,9 @@ class SearchResult(models.Model):
     label = models.CharField(max_length=100, default=0)
     relevance = models.CharField(blank=True, max_length=1, default='')
 
+    predicted_relevance = models.CharField(blank=True, max_length=1, default='')
+    predicted_score = models.FloatField(blank=True, null=True)
+
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
