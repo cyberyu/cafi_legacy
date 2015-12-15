b15fd11a3bb5887c33e3974f29bb7ed684ab8ba4
Mingfeng Yang <mfyang@gmail.com>
2015-12-14 22:57:04 -0500

    update requirements.txt to use bs4; remove personal files from repo

--- backend/nltk_script.sh
+++
@@ -1,6 +0,0 @@
-export CLASSPATH="/home/shiyu/cafi_tanmoy/backend/google/ner/models"
-echo $CLASSPATH
-export STANFORD_MODELS="/home/shiyu/cafi_tanmoy/backend/google/ner/models"
-echo $STANFORD_MODELS 
-export JAVAHOME="/usr/lib/jvm/java-8-oracle"
-echo $JAVAHOME
